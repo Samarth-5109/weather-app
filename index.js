4a4a91b9734656afbb1094a54439abfe4eb48aeb
Samarth Bhardwaj <samarthb5109@gmail.com>
2024-05-31 18:27:02 +0530

    added JS

---
+++ index.js
@@ -0,0 +1,30 @@
+const inputText = document.querySelector(".city-input");
+const searchBtn = document.querySelector(".btn");
+const temperature = document.querySelector(".temp");
+const city = document.querySelector(".city");
+const humidity = document.querySelector(".humid-perc");
+const wind = document.querySelector(".wind-speed");
+
+const serverKey = "0c3fe26502b28ecc15a3ae22e25cd1be";
+const serverUrl =
+  "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&units=metric";
+
+function getWeatherUrl(city) {
+  return serverUrl + `&appid=${serverKey}` + `&q=${city}`;
+}
+
+searchBtn.addEventListener("click", function clickEventhandler() {
+  const cityName = inputText.value;
+  fetch(getWeatherUrl(cityName))
+    .then((response) => response.json())
+    .then((json) => {
+      const cityTemp = json.main.temp;
+      const cityHumidity = json.main.humidity;
+      const cityWindSpeed = json.wind.speed;
+
+      temperature.innerText = cityTemp + "°C";
+      city.innerText = cityName;
+      humidity.innerText = cityHumidity + "%";
+      wind.innerText = cityWindSpeed + "km/h";
+    });
+});
