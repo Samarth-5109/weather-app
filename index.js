308f09b53f78865a1c430f47bb1820520eca426e
Samarth Bhardwaj <samarthb5109@gmail.com>
2024-06-04 22:12:27 +0530

    added some JS

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const temperature = document.querySelector(".temp");
 const city = document.querySelector(".city");
 const humidity = document.querySelector(".humid-perc");
 const wind = document.querySelector(".wind-speed");
-
+const weatherDetails = document.querySelector(".weather-details");
 const serverKey = "0c3fe26502b28ecc15a3ae22e25cd1be";
 const serverUrl =
   "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&units=metric";
@@ -15,16 +15,21 @@ function getWeatherUrl(city) {
 
 searchBtn.addEventListener("click", function clickEventhandler() {
   const cityName = inputText.value;
-  fetch(getWeatherUrl(cityName))
-    .then((response) => response.json())
-    .then((json) => {
-      const cityTemp = json.main.temp;
-      const cityHumidity = json.main.humidity;
-      const cityWindSpeed = json.wind.speed;
+  if (cityName === "") {
+    weatherDetails.style.display = "none";
+  } else {
+    weatherDetails.style.display = "block";
+    fetch(getWeatherUrl(cityName))
+      .then((response) => response.json())
+      .then((json) => {
+        const cityTemp = json.main.temp;
+        const cityHumidity = json.main.humidity;
+        const cityWindSpeed = json.wind.speed;
 
-      temperature.innerText = cityTemp + "°C";
-      city.innerText = cityName;
-      humidity.innerText = cityHumidity + "%";
-      wind.innerText = cityWindSpeed + "km/h";
-    });
+        temperature.innerText = cityTemp + "°C";
+        city.innerText = cityName;
+        humidity.innerText = cityHumidity + "%";
+        wind.innerText = cityWindSpeed + "km/h";
+      });
+  }
 });
